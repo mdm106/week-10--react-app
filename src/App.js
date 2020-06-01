64188d08ee12ce1c760a543cf10330d7ac65e98c
mdm106 <mads.mcmahon@googlemail.com>
2020-06-01 16:22:12 +0100

    Passing data up challenge 2: Form component which passes up name and email inputs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Squares from './squares/Squares';
 import SignUp from './signup/SignUp';
 import Button from './button/Button';
+import Form from './form/Form';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Squares />
       <SignUp />
       <Button handleUpdate={ console.log } />
+      <Form handleSubmit={ console.log } />
     </>
   );
 }
---
+++ src/form/Form.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "",
+            email: "",
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleName = this.handleName.bind(this);
+        this.handleEmail = this.handleEmail.bind(this);
+    }
+
+    handleName(e) {
+        this.setState({ name: e.currentTarget.value });
+    }
+
+    handleEmail(e) {
+        this.setState({ email: e.currentTarget.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        let { handleSubmit } = this.props;
+        handleSubmit(this.state);
+        this.setState({ name: "", email: ""});
+    }
+
+    render() {
+        let { name, email } = this.state;
+
+        return (
+            <form>
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input className="form-control"
+                           id="name"
+                           name="name"
+                           type="text"
+                           value={ name }
+                           onChange={(e) => this.handleName(e)} />
+                    <label htmlFor="email">Email</label>
+                    <input className="form-control"
+                           id="email"
+                           name="email"
+                           type="text"
+                           value={ email }
+                           onChange={(e) => this.handleEmail(e)} />
+                </div>
+                <button className="btn btn-primary" type="submit" onClick={(e) => this.handleSubmit(e)}>Submit</button>
+            </form>
+        )
+    }
+}
+
+export default Form;

a9334eb75ef5b10a4a8a314742ee5f50f9d354c3
mdm106 <mads.mcmahon@googlemail.com>
2020-06-02 10:25:40 +0100

    Challenge 3 refactoring to have only one handlechange method and making corrections so the props don't get changed when the counter buttons are clicked

--- src/max/Max.js
+++ src/max/Max.js
@@ -5,37 +5,31 @@ import Counter from "./Counter";
 class Max extends Component {
     constructor(props) {
         super(props);
-        this.state = { numbers: this.props.count };
+        this.state = { numbers: props.count.map((number) => number) };//use a map as just using this.props.count means the count gets updated
     }
 
-    handleClickPlus(index) {
-        let numbers = this.state.numbers;
+    handleChange(index, step) {
+        let values = this.state.numbers;
         let { max } = this.props;
-        let currentNumber = numbers[index];
-        numbers[index] = currentNumber < max ? currentNumber + 1 : currentNumber;
-        this.setState({ numbers: numbers });
+        let currentNumber = values[index];
+        let newNumber = currentNumber + step;
+        values[index] = newNumber <= max && newNumber >= 0 ? newNumber : currentNumber; //to prevent exceeding max or going below 0
+        this.setState({ numbers: values });
     }
-
-    handleClickMinus(index) {
-        let numbers = this.state.numbers;
-        let currentNumber = numbers[index];
-        numbers[index] = currentNumber > 0 ? currentNumber - 1 : currentNumber;
-        this.setState({ numbers: numbers });
-    }
-
+    
     render() {
         let { count } = this.props;
 
         let { numbers } = this.state;
-        let highest = Math.max(...numbers);
+        let highest = Math.max(...numbers); // to return the highest value of numbers
 
         return (
             <> 
             {count.map((number, index) => (
                 <Counter key= { index } 
-                        counter= { number }
-                        handleClickPlus={ () => this.handleClickPlus(index) }
-                        handleClickMinus={ () => this.handleClickMinus(index) }
+                        counter= { numbers[index] }
+                        handleClickPlus={ () => this.handleChange(index, 1) }
+                        handleClickMinus={ () => this.handleChange(index, -1) }
                         highlight={ numbers[index] === highest }     />
             )) }
             

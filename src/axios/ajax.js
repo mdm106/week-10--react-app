c04863763daad141155587c4f639afa644383945
mdm106 <mads.mcmahon@googlemail.com>
2020-06-02 14:49:28 +0100

    02-Api-04 Adding tag and 02-Api-05 Deleting an article

--- src/axios/ajax.js
+++ src/axios/ajax.js
@@ -16,6 +16,17 @@ axios.get("/articles").then(({ data }) => {
 //     console.log(data.data.id);
 // })
 
+//Challenge 3
 axios.get("/articles/84").then(({ data }) => {
     console.log(data.data);
 })
+
+//Challenge 4
+axios.patch("/articles/87", {
+    tags: ["hot", "sunny", "day"],
+}).then(({ data }) => {
+    console.log(data.data.tags);
+})
+
+//Challenge 5
+axios.delete("/articles/82").then(console.log("Deleted"));

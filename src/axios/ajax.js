86b64f4e9bb2243554d5970e1cf3d7736caa1bd5
mdm106 <mads.mcmahon@googlemail.com>
2020-06-02 14:32:23 +0100

    02-Api-02 Creating a new article using axios and console.log it's ID

--- src/axios/ajax.js
+++ src/axios/ajax.js
@@ -6,3 +6,12 @@ import axios from "./axios";
 axios.get("/articles").then(({ data }) => {
     console.log(data.data);
 });
+
+//Challenge 2
+axios.post("/articles", {
+    title: "Tuesday afternoon",
+    content: "It was very hot",
+    tags: ["hot", "sunny"],
+}).then(({ data }) => {
+    console.log(data.data.id);
+})

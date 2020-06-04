6650e28ebfa4ca342771065c3f7e0036150df1d9
mdm106 <mads.mcmahon@googlemail.com>
2020-06-04 12:45:41 +0100

    Changed names of the event handler

--- src/hooks/Counter.js
+++ src/hooks/Counter.js
@@ -3,16 +3,16 @@ import React, { useState } from 'react';
 const Counter = ({ initial, max }) => {
     const [counter, setCounter] = useState(initial);
 
-    const plusCounter = () => setCounter(counter < max ? counter + 1 : counter);
+    const handlePlus = () => setCounter(counter < max ? counter + 1 : counter); //event handler
 
-    const minusCounter = () => setCounter(counter > 0 ? counter - 1 : counter);
+    const handleMinus = () => setCounter(counter > 0 ? counter - 1 : counter); //event handler
 
     return (
         <>
             <p>Counter: {counter} </p>
             <div className="d-flex flex-row">
-                <button className="btn-success" onClick={ plusCounter }>+</button>
-                <button className="btn-danger" onClick={ minusCounter }>-</button>
+                <button className="btn-success" onClick={ handlePlus }>+</button>
+                <button className="btn-danger" onClick={ handleMinus }>-</button>
             </div>
         </>
     )

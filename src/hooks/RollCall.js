7841ab5a9d8bd8beaa5a8e1dbd95e3ee90cc389f
mdm106 <mads.mcmahon@googlemail.com>
2020-06-04 11:28:44 +0100

    Removing unnecessary {

--- src/hooks/RollCall.js
+++ src/hooks/RollCall.js
@@ -3,8 +3,7 @@ import React, { useState } from 'react';
 const RollCall = ({ names }) => {
     const [index, setIndex] = useState(0);
 
-    const updateIndex = () => { setIndex(index < names.length - 1 ? index + 1 : 0 )
-    }
+    const updateIndex = () => setIndex(index < names.length - 1 ? index + 1 : 0 );
 
     return (
         <>  

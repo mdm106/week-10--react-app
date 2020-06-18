9f6a6ba48e7cc0bc201a97f8541b7cbac1f11be0
mdm106 <mads.mcmahon@googlemail.com>
2020-06-18 22:03:42 +0100

    Updates

---
+++ precache-manifest.499b1d89f5ddec57675dc364c858d834.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8162819256b0856c12cd2f523e1624d9",
+    "url": "/week-10--react-app/index.html"
+  },
+  {
+    "revision": "dee1d34da459bd35b104",
+    "url": "/week-10--react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "46773f86ac4836ae506f",
+    "url": "/week-10--react-app/static/js/2.cc1f5720.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/week-10--react-app/static/js/2.cc1f5720.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dee1d34da459bd35b104",
+    "url": "/week-10--react-app/static/js/main.e7519e02.chunk.js"
+  },
+  {
+    "revision": "d388327d301e6636b0f1",
+    "url": "/week-10--react-app/static/js/runtime-main.bead9fe3.js"
+  }
+]);
